f99af4a7626235acf46007bfb36762c0795f2a90
Mohamed Elbaruni <mohamed.elbaruni@gmail.com>
2022-03-09 02:51:01 +0200

    commit

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -12,7 +12,7 @@ export const config = {
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   jwt: {
     secret: process.env.JWT_SECRET,
